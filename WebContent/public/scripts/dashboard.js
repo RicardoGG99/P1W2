0e9073169fe9c75c3eba39155cfc4cb19dcd2a0c
RicardoGG99 <36855481+RicardoGG99@users.noreply.github.com>
2021-03-05 22:40:08 -0400

    Add files via upload

--- WebContent/public/scripts/dashboard.js
+++ WebContent/public/scripts/dashboard.js
@@ -14,14 +14,13 @@ form.addEventListener('submit', function(e){
     fetch('https://p1-w2.herokuapp.com/Sesion', datos)
     .then( res => res.json())
     .then( data => {
-        console.log(data.cedula)
-        document.getElementById("h1").value = `Bienvenido ${data.nombre}`;
-        document.getElementById("cedula").value = data.cedula;
-        document.getElementById("nombre").value = data.nombre;
-        document.getElementById("apellido").value = data.apellido;
-        document.getElementById("fdn").value = data.fdn;
-        document.getElementById("password").value = data.password;
-        document.getElementById("email").value = data.email;
+        document.getElementById("h1").innerHTML = `Bienvenido ${data.nombre}`;
+        document.getElementById("cedula").innerHTML = data.cedula;
+        document.getElementById("nombre").innerHTML = data.nombre;
+        document.getElementById("apellido").innerHTML = data.apellido;
+        document.getElementById("fdn").innerHTML = data.fdn;
+        document.getElementById("password").innerHTML = data.password;
+        document.getElementById("email").innerHTML = data.email;
         window.open('https://p1-w2.herokuapp.com/public/views/logged.html', "_self");
     })
     .catch(error => console.error());
