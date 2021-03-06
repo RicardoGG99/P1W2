86d6ddbeb8df6ded329dcadb8f054bc0deab9c2f
RicardoGG99 <36855481+RicardoGG99@users.noreply.github.com>
2021-03-05 23:06:36 -0400

    Add files via upload

--- WebContent/public/scripts/dashboard.js
+++ WebContent/public/scripts/dashboard.js
@@ -14,7 +14,6 @@ form.addEventListener('submit', function(e){
     fetch('https://p1-w2.herokuapp.com/Sesion', datos)
     .then( res => res.json())
     .then( data => {
-        console.log(data.cedula + "" + data.nombre + "" + data.apellido + "" + data.fdn + "" + data.password + "" + data.email)
         document.getElementById("h1").innerHTML = `Bienvenido ${data.nombre}`;
         document.getElementById("cedula").innerHTML = data.cedula;
         document.getElementById("nombre").innerHTML = data.nombre;
